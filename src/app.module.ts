2545583c1bac85b3224256f4f745c4f2afac9fc4
Mendrika <mendrikaitokiuana@gmail.com>
2024-11-30 22:02:02 +0300

    ✨ Database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DriversModule } from './drivers/drivers.module';
-import { ClientModule } from './client/client.module';
-import { RideModule } from './ride/ride.module';
-import { PaymentModule } from './payment/payment.module';
-import { NotificationModule } from './notification/notification.module';
+import { ClientsModule } from './client/client.module';
+import { RidesModule } from './ride/ride.module';
+import { PaymentsModule } from './payment/payment.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NotificationsModule } from './notification/notification.module';
 
 @Module({
-  imports: [UsersModule, DriversModule, ClientModule, RideModule, PaymentModule, NotificationModule],
+  imports: [
+    UsersModule,
+    DriversModule,
+    ClientsModule,
+    RidesModule,
+    PaymentsModule,
+    NotificationsModule,
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
