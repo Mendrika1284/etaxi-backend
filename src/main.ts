ecf807fdac982439f6a9ad9e7e23cbb91037cfbc
Mendrika1284 <68342672+Mendrika1284@users.noreply.github.com>
2025-07-18 19:14:50 +0300

    ✨ Add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('e-taxi API')
+    .setDescription('Documentation de l’API e-taxi')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
